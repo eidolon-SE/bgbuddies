8670feac027d22c5ed1e9958e7724cd4243fa895
samuel p. <>
2024-12-21 20:06:11 +1100

    wizard: fixed unplayable bug, added undo turn feature

--- src/views/Wizard/WizardScoreSheetView.vue
+++ src/views/Wizard/WizardScoreSheetView.vue
@@ -3,6 +3,7 @@
     import NameField from '../../components/NameField.vue'
     import RangeInput from '../../components/RangeInput.vue'
 
+    const gameOver = ref(false)
     const numPlayers = ref(3)
     const playersData = ref([])
     const numTurns = ref(0)
@@ -10,33 +11,51 @@
     const gameStarted = ref(false)
 
     function newGame() {
+        gameOver.value = false
         numTurns.value = 60 / numPlayers.value
         currentTurn.value = 1
         
         playersData.value = []
         for (let i = 0; i < numPlayers.value; i++) {
-            playersData.value.push({name: 'Player', guess: 0, tricks: 0, score: 0 })
+            playersData.value.push({ guess: 0, tricks: 0, score: 0, history: [] })
         }
 
         gameStarted.value = true
     }
 
     function takeTurn() {
-        if (currentTurn.value < numTurns.value) {
-            currentTurn.value++
-            playersData.value.forEach((el) => {
-                if (el.guess === el.tricks) {
-                    let score = 20 + (10 * el.tricks)
-                    el.score += score
-                } else {
-                    let score = 10 * (Math.max(el.guess, el.tricks) - Math.min(el.guess, el.tricks))
-                    el.score -= score
-                }
+        playersData.value.forEach((el) => {
+            el.history.push({ guess: el.guess, tricks: el.tricks, score: el.score })
 
-                el.guess = 0
-                el.tricks = 0
-            })
+            if (el.guess === el.tricks) {
+                let score = 20 + (10 * el.tricks)
+                el.score += score
+            } else {
+                let score = 10 * (Math.max(el.guess, el.tricks) - Math.min(el.guess, el.tricks))
+                el.score -= score
+            }
+
+            el.guess = 0
+            el.tricks = 0
+        })
+
+        currentTurn.value++
+        
+        if (currentTurn.value > numTurns.value) {
+            gameOver.value = true
         }
+    }
+
+    function undoTurn() {
+        gameOver.value = false
+        currentTurn.value--
+
+        playersData.value.forEach((el) => {
+            let lastTurn = el.history.pop()
+            el.guess = lastTurn.guess
+            el.tricks = lastTurn.tricks
+            el.score = lastTurn.score
+        })
     }
 </script>
 
@@ -49,21 +68,23 @@
         <input type="button" class="button" @click="newGame()" value="New Game">
     </div>
     <div v-if="gameStarted" class="game">
-        <div>Turn {{ currentTurn }} / {{ numTurns }}</div>
+        <div v-if="gameOver">Game Over</div>
+        <div v-else>Turn {{ currentTurn }} / {{ numTurns }}</div>
         <div class="score-grid">
             <div class="grid-title">Players</div>
             <div class="grid-title">Guess</div>
             <div class="grid-title">Tricks</div>
             <div class="grid-title">Total Score</div>
             <template v-for="(data, index) in playersData" :key="index">
-                <NameField :initialName="data.name" />
-                <div><input type="number" v-model="data.guess" min=0 :max="currentTurn"></div>
-                <div><input type="number" v-model="data.tricks" min=0 :max="currentTurn"></div>
+                <NameField initialName="Player" />
+                <div><input type="number" v-model="data.guess" min=0 :max="currentTurn" :disabled="gameOver"></div>
+                <div><input type="number" v-model="data.tricks" min=0 :max="currentTurn" :disabled="gameOver"></div>
                 <div>{{ data.score }}</div>
             </template>
         </div>
         <div class="inputs">
-            <button class="button" @click="takeTurn">End Turn</button>
+            <button class="button" :disabled="currentTurn <= 1" @click="undoTurn">Undo Last Turn</button>
+            <button class="button" :disabled="gameOver" @click="takeTurn">End Turn</button>
         </div>
     </div>
 </template>
@@ -72,6 +93,7 @@
 .score-grid {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
+    gap: 0.5rem;
 }
 
 .grid-title {
