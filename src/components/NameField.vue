3ea12734c6bd113ac12ced1bc578d11e4b7d8534
samuel p. <>
2024-12-24 20:41:09 +1100

    started scythe score calc

---
+++ src/components/NameField.vue
@@ -0,0 +1,55 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const props = defineProps(['initialName'])
+
+    const name = ref(props.initialName)
+    const showInput = ref(false)
+
+    function save() {
+        if (name.value.match("^[A-Za-z0-9_]")) {
+            showInput.value = false
+        }
+    }
+</script>
+<template>
+    <div class="container">
+        <div @click="showInput = true">
+            {{ name }}
+        </div>
+        <div v-if="showInput" class="modal">
+            <div class="modal-inner">
+                <input type="text" v-model="name" pattern="[A-za-z0-9_]">
+                <input type="button" class="button" value="Save" @click="save">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.modal {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    gap: 0.25rem;
+    height: 100%;
+    width: 100%;
+    background: #ffffffcc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+.modal-inner {
+    margin: 1rem;
+    display: flex;
+    gap: 0.5rem;
+}
+
+input[type=text] {
+    font-size: 1.5rem;
+    width: 10rem;
+}
+</style> 
--- src/views/Scythe/ScytheScoreCalculatorView.vue
+++ src/views/Scythe/ScytheScoreCalculatorView.vue
@@ -1,3 +1,25 @@
+<script setup>
+    import { ref, onMounted } from 'vue'
+
+    const numPlayers = ref(7)
+    const playerData = ref([])
+
+    function buildPlayerData() {
+        for (let i = 0; i < numPlayers.value; i++) {
+            playerData.value.push({
+                popularity: 0,
+                coins: 0,
+                stars: 0,
+                territories: 0,
+                resources: 0,
+                structures: 0
+            })
+        }
+    }
+
+    onMounted(buildPlayerData)
+</script>
+
 <template>
-    Scythe Score Calculator
+    {{ playerData }}
 </template>
